9443b51175a0959786268fb928fb26401eee8569
klarok <karaylu@gmail.com>
2018-11-05 14:26:22 -0800

    stringifyJSON passes tests

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,33 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  let type = typeof obj;
+  let isArray = Array.isArray(obj);
+  //check if number
+  if (type === 'number' || type === 'boolean' || obj === null) {
+    return '' + obj;
+  } else if (type === 'string') {
+    return '"' + obj + '"';
+  } else if (isArray) {
+    let arrayString = '[';
+    for (let i = 0; i < obj.length; i++) {
+      arrayString += stringifyJSON(obj[i]);
+      if (i < obj.length - 1) {
+        arrayString += ',';
+      }
+    }
+    arrayString += ']';
+    return arrayString;
+  } else if (type === 'object') {
+    let objectString = '{';
+    for (let key in obj) {
+      if (!(typeof obj[key] === 'function' || obj[key] === undefined)) {
+        objectString += stringifyJSON(key) + ':' + stringifyJSON(obj[key]) + ',';
+      } 
+    }
+    objectString = (objectString.length === 1) ? objectString : objectString.slice(0, objectString.length - 1);
+    objectString += '}';
+    return objectString;
+  }
+  return undefined;
 };
